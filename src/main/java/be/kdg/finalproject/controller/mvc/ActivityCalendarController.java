c0d306b2f8feddd80a2c12b159dc8e302f575858
MitrovicRelja <relja.mitrovic@student.kdg.be>
2023-04-04 20:22:22 +0200

    working on seeding some data for calendar activites using entity factory and database seeder. added service package and classes and repository also. Having issue running the project because of env file

--- src/main/java/be/kdg/finalproject/controller/mvc/ActivityCalendarController.java
+++ src/main/java/be/kdg/finalproject/controller/mvc/ActivityCalendarController.java
@@ -1,16 +1,33 @@
 package be.kdg.finalproject.controller.mvc;
 
+import be.kdg.finalproject.domain.activities.CalendarActivity;
+import be.kdg.finalproject.domain.user.User;
+import be.kdg.finalproject.exceptions.EntityNotFoundException;
+import be.kdg.finalproject.municipalities.MunicipalityId;
+import be.kdg.finalproject.service.CalendarActivities.CalendarActivitiesService;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class ActivityCalendarController {
+	private final Logger logger = org.slf4j.LoggerFactory.getLogger(ActionPointController.class);
+	private final CalendarActivitiesService calendarActivitiesService;
 
-	@GetMapping ("/activities-calendar")
-	public ModelAndView showCalendar() {
-		ModelAndView modelAndView = new ModelAndView("activities-calendar");
-		//modelAndView.addObject("activities-schedule", attributeValue);
-		return modelAndView;
+	public ActivityCalendarController(CalendarActivitiesService calendarActivitiesService) {this.calendarActivitiesService = calendarActivitiesService;}
+	@GetMapping ("/calendar-activities")
+	public ModelAndView showActivitiesCalendar(@MunicipalityId Long municipalityId, @ModelAttribute ("authUser") User user) {
+		if (municipalityId == null) {
+			logger.debug("No municipality ID found");
+			throw new EntityNotFoundException("Not found");
+		}
+		List<CalendarActivity> activities = calendarActivitiesService.getAllCalendarActivities();
+		logger.debug("Calendar activities found: {}", activities);
+		return new ModelAndView("calendar-activities")
+				.addObject("activities", activities);
 	}
 }
---
+++ src/main/java/be/kdg/finalproject/repository/CalendarActivity/CalendarActivityRepository.java
@@ -0,0 +1,9 @@
+package be.kdg.finalproject.repository.CalendarActivity;
+
+import be.kdg.finalproject.domain.activities.CalendarActivity;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CalendarActivityRepository  extends CrudRepository<CalendarActivity, Long> {
+
+}
