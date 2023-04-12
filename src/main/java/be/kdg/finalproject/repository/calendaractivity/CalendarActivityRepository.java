ced2dad4c49ca7962139c9d288b1bbcf11a2d21a
MitrovicRelja <relja.mitrovic@student.kdg.be>
2023-04-12 19:34:54 +0200

    ...do I need to add time also ? probably yes and the DatabaseSeeder class do I need to getMunicipality()

--- src/main/java/be/kdg/finalproject/repository/calendaractivity/CalendarActivityRepository.java
+++ src/main/java/be/kdg/finalproject/repository/calendaractivity/CalendarActivityRepository.java
@@ -1,9 +1,11 @@
-package be.kdg.finalproject.repository.calendaractivities;
+package be.kdg.finalproject.repository.calendaractivity;
 
 import be.kdg.finalproject.domain.activities.CalendarActivity;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CalendarActivityRepository extends CrudRepository<CalendarActivity, Long> {
 
 }
