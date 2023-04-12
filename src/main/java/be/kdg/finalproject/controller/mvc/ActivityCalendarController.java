29845c768ed1657dcc96e5cd2289d3e9f47ff249
MitrovicRelja <relja.mitrovic@student.kdg.be>
2023-04-12 18:44:59 +0200

    ...working on it

--- src/main/java/be/kdg/finalproject/controller/mvc/ActivityCalendarController.java
+++ src/main/java/be/kdg/finalproject/controller/mvc/ActivityCalendarController.java
@@ -19,12 +19,36 @@ public class ActivityCalendarController {
 	private final CalendarActivitiesService calendarActivitiesService;
 
 	public ActivityCalendarController(CalendarActivitiesService calendarActivitiesService) {this.calendarActivitiesService = calendarActivitiesService;}
+//	@GetMapping ("/calendar-activities")
+//	public ModelAndView showActivitiesCalendar(@MunicipalityId Long municipalityId, @ModelAttribute ("authUser") User user) {
+//		if (municipalityId == null) {
+//			logger.debug("No municipality ID found");
+//			throw new EntityNotFoundException("Not found");
+//		}
+//		List<CalendarActivity> activities = calendarActivitiesService.getAllCalendarActivities();
+//		logger.debug("Calendar activities found: {}", activities);
+//		return new ModelAndView("calendar-activities")
+//				.addObject("activities", activities);
+//	}
+
+
+	//Municip is optional in this method
+//	@GetMapping("/calendar-activities")
+//	public ModelAndView showActivitiesCalendar(@MunicipalityId(required = false) Long municipalityId, @ModelAttribute("authUser") User user) {
+//		List<CalendarActivity> activities;
+//		if (municipalityId == null) {
+//			activities = calendarActivitiesService.getAllCalendarActivities();
+//		} else {
+//			activities = calendarActivitiesService.getAllCalendarActivitiesForMunicipality(municipalityId);
+//		}
+//		logger.debug("Calendar activities found: {}", activities);
+//		return new ModelAndView("calendar-activities")
+//				.addObject("activities", activities);
+//	}
+
+	//FOR TESTING
 	@GetMapping ("/calendar-activities")
-	public ModelAndView showActivitiesCalendar(@MunicipalityId Long municipalityId, @ModelAttribute ("authUser") User user) {
-		if (municipalityId == null) {
-			logger.debug("No municipality ID found");
-			throw new EntityNotFoundException("Not found");
-		}
+	public ModelAndView showActivitiesCalendar() {
 		List<CalendarActivity> activities = calendarActivitiesService.getAllCalendarActivities();
 		logger.debug("Calendar activities found: {}", activities);
 		return new ModelAndView("calendar-activities")
