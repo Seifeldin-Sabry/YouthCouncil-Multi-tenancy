7e8a6ca2bc5a6c6b4eb9c079d8a80ca386f39957
MitrovicRelja <relja.mitrovic@student.kdg.be>
2023-04-13 23:00:21 +0200

    ...working on it

---
+++ src/main/java/be/kdg/finalproject/controller/api/dto/get/CalendarActivityDTO.java
@@ -0,0 +1,13 @@
+package be.kdg.finalproject.controller.api.dto.get;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class CalendarActivityDTO {
+
+
+}
