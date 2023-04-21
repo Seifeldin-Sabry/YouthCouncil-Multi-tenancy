5f5877352ec8e3e25c8d3f6c976e35bfd7c83890
MitrovicRelja <relja.mitrovic@student.kdg.be>
2023-04-21 14:01:36 +0200

    ...fixing gitlab code suggestions/comments

--- src/main/java/be/kdg/finalproject/controller/mvc/ActivityCalendarController.java
+++ src/main/java/be/kdg/finalproject/controller/mvc/ActivityCalendarController.java
@@ -4,8 +4,8 @@ import be.kdg.finalproject.domain.activities.CalendarActivity;
 import be.kdg.finalproject.exceptions.EntityNotFoundException;
 import be.kdg.finalproject.municipalities.MunicipalityId;
 import be.kdg.finalproject.service.calendaractivities.CalendarActivitiesService;
-import be.kdg.finalproject.service.municipality.MunicipalityService;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,17 +14,14 @@ import java.util.List;
 
 @Controller
 public class ActivityCalendarController {
-	private final Logger logger = org.slf4j.LoggerFactory.getLogger(ActivityCalendarController.class);
+	private final Logger logger = LoggerFactory.getLogger(ActivityCalendarController.class);
 	private final CalendarActivitiesService calendarActivitiesService;
-	private final MunicipalityService municipalityService;
 
-	public ActivityCalendarController(CalendarActivitiesService calendarActivitiesService,
-	                                  MunicipalityService municipalityService) {
+	public ActivityCalendarController(CalendarActivitiesService calendarActivitiesService) {
 		this.calendarActivitiesService = calendarActivitiesService;
-		this.municipalityService = municipalityService;
 	}
 
-	@GetMapping ("/calendar-activities") //
+	@GetMapping ("/calendar-activities")
 	public ModelAndView showActivitiesCalendar(@MunicipalityId Long municipalityId) {
 		List<CalendarActivity> activities;
 		if (municipalityId == null) {
